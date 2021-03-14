370cd88582789cef92cecc2a11b999a06d30dbe9
Celestino Agudo <celestinoagudo07@gmail.com>
2021-03-14 23:46:37 +0800

    Initial development on menu contents; refactored some JS codes to decrease duplicate codes

--- fine-dinning/js/script.js
+++ fine-dinning/js/script.js
@@ -1,12 +1,14 @@
-/**
- * Utility method to collapse the navigation menu when the user clicks 
- * anywhere outside the toggler button 
- */
 (function (global){
+    /**
+     * Utility method to collapse the navigation menu when the user clicks 
+     * anywhere outside the toggler button 
+     */
     var myNavBarToggler = document.getElementById('navBarToggler');
     myNavBarToggler.addEventListener('blur', function(event){
+
         var screenWidth = window.innerWidth;
         if (screenWidth <= 991) {
+            
             var navigationMenu = document.getElementById('navbarSupportedContent');
             const bsCollapse = new bootstrap.Collapse(navigationMenu);
             bsCollapse.toggle();
@@ -48,25 +50,13 @@
      */
     fineDinning.loadHomePage = function(){
 
-        var aboutPageIndicator = document.getElementById('at-about');
-        aboutPageIndicator.classList.remove('active');
-
-        var menuPageIndicator = document.getElementById('at-menu');
-        menuPageIndicator.classList.remove('active');
-        
-        var newsPageIndicator = document.getElementById('at-news');
-        newsPageIndicator.classList.remove('active');
-
-        var contactPageIndicator = document.getElementById('at-contacts');
-        contactPageIndicator.classList.remove('active');
-
-        var homePageIndicator = document.getElementById('at-home');
-        homePageIndicator.classList.add('active');
+        activateTabAndDeactivateTheRest('at-home');
 
         var homeHTMLSnippet = "../fine-dinning/snippets/home-snippet.html";
         $ajaxUtils.sendGetRequest(homeHTMLSnippet, 
             function(responseText){
-                document.querySelector("#main-content").innerHTML = responseText;
+                insertHTML("#main-content",responseText);
+                // document.querySelector("#main-content").innerHTML = responseText;
             },
             false
         );
@@ -77,25 +67,13 @@
      */
     fineDinning.loadAboutPage = function(){
 
-        var homePageIndicator = document.getElementById('at-home');
-        homePageIndicator.classList.remove('active');
-
-        var menuPageIndicator = document.getElementById('at-menu');
-        menuPageIndicator.classList.remove('active');
-        
-        var newsPageIndicator = document.getElementById('at-news');
-        newsPageIndicator.classList.remove('active');
-
-        var contactPageIndicator = document.getElementById('at-contacts');
-        contactPageIndicator.classList.remove('active');
-
-        var aboutPageIndicator = document.getElementById('at-about');
-        aboutPageIndicator.classList.add('active');
+        activateTabAndDeactivateTheRest('at-about');
 
         var aboutHTMLSnippet = "../fine-dinning/snippets/about-snippet.html";
         $ajaxUtils.sendGetRequest(aboutHTMLSnippet, 
             function(responseText){
-                document.querySelector("#main-content").innerHTML = responseText;
+                // document.querySelector("#main-content").innerHTML = responseText;
+                insertHTML("#main-content",responseText);
             },
             false
         );
@@ -105,29 +83,89 @@
      * Utility method to load the menu contents into the main content
      */
     fineDinning.loadMenuPage = function(){
-        var homePageIndicator = document.getElementById('at-home');
-        homePageIndicator.classList.remove('active');
-        
-        var newsPageIndicator = document.getElementById('at-news');
-        newsPageIndicator.classList.remove('active');
 
-        var contactPageIndicator = document.getElementById('at-contacts');
-        contactPageIndicator.classList.remove('active');
-
-        var aboutPageIndicator = document.getElementById('at-about');
-        aboutPageIndicator.classList.remove('active');
-
-        var menuPageIndicator = document.getElementById('at-menu');
-        menuPageIndicator.classList.add('active');
+        activateTabAndDeactivateTheRest('at-menu');
 
         var menuHTMLSnippet = "../fine-dinning/snippets/menu-snippet.html";
         $ajaxUtils.sendGetRequest(menuHTMLSnippet, 
             function(responseText){
-                document.querySelector("#main-content").innerHTML = responseText;
+
+                insertHTML("#main-content",responseText);
             },
             false
         );
 
+        var foodMenuDirectory = "../fine-dinning/resources/json/menu.json";
+        $ajaxUtils.sendGetRequest(foodMenuDirectory, 
+            function(responseObjects){
+                renderFoodCardsForSchedule("breakfast", responseObjects);    
+            },
+            true
+        );
+    }
+
+    /**
+     * utility method to activate new tab and deactivate the rest
+     * 
+     * @param {String} tabToActivate indicates the tab to activate
+     */
+    var activateTabAndDeactivateTheRest = function(tabToActivate){
+        const tabs = ['at-home', 'at-about', 'at-menu', 'at-news', 'at-contacts'];
+        const activeState = 'active';
+
+        var activePageIndicator = document.getElementById(tabToActivate);
+        activePageIndicator.classList.add(activeState);
+
+        for(var counter = 0; counter < tabs.length; ++counter){
+            if(tabs[counter] !== tabToActivate){
+                var inactivePageIndicator = document.getElementById(tabs[counter]);
+                inactivePageIndicator.classList.remove(activeState);
+            }
+        }
+    }
+
+    /**
+     * utility method to generate food cards for passed schedule
+     * 
+     * @param {String} schedule (breakfast, lunch, dinner)
+     * @param {Object} responseObjects (foods offered by the restaurant)
+     */
+    var renderFoodCardsForSchedule = function(schedule, responseObjects){
+        var foodContents = "";
+        var selector = "";
+        var key = "";
+
+        switch(schedule){
+            case "breakfast" : {
+                key = "breakfast";
+                selector = "#breakfastBody"
+                break;
+            }
+            case "lunch" : {
+                key = "lunch";
+                selector = "#lunchBody";
+                break;
+            }
+            case "dinner" : {
+                key = "dinner";
+                selector = "#dinnerBody";
+                break;
+            }
+        }
+
+        for(var element in responseObjects[key]){
+
+            foodContents += "<div>"
+            foodContents += "<div class=\"card h-100\">";
+            foodContents += "<img src=\"" + responseObjects[key][element].directory 
+                                            +"\" class=\"card-img-top\" alt=\"...\">";
+            foodContents += "<div class=\"card-body\">"
+            foodContents += "<h5 class=\"card-title\">" + responseObjects[key][element].name + "</h5>"
+            foodContents += "<p class=\"card-text\">" +  responseObjects[key][element].description + "</p>"
+            foodContents += "</div></div></div>";
+        }
+
+        insertHTML(selector, foodContents);
     }
 
     /**
