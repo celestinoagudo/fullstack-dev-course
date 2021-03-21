49044f827c20d08f78ca93633650677895df87e4
Celestino Agudo <celestinoagudo07@gmail.com>
2021-03-21 23:16:32 +0800

    Coded specials page and adjusted some margins of page elements

--- fine-dinning/js/script.js
+++ fine-dinning/js/script.js
@@ -2,9 +2,9 @@
  * Utility method to collapse the navigation menu when the user clicks 
  * anywhere outside the toggler button
  */
-(function (global){
+(function (global) {
     var myNavBarToggler = document.getElementById('navBarToggler');
-    myNavBarToggler.addEventListener('blur', function(event){
+    myNavBarToggler.addEventListener('blur', function (event) {
 
         var screenWidth = window.innerWidth;
         if (screenWidth <= 991) {
@@ -17,15 +17,17 @@
 })(window);
 
 
-(function(global){
+(function (global) {
     //setup namespace for the project
     var fineDinning = {};
 
-        /**
+    var foodMenuDirectory = "../fine-dinning/resources/json/menu.json";
+
+    /**
      * Call the necessary methods on page load (before images or CSS) 
      */
-    document.addEventListener("DOMContentLoaded", function(){
-         // collapseNavigationMenuOnBlur();
+    document.addEventListener("DOMContentLoaded", function () {
+        // collapseNavigationMenuOnBlur();
         showLoading("#main-content");
         fineDinning.loadHomePage();
     });
@@ -36,9 +38,9 @@
      * @param {*} selector the selector
      * @param {*} html HTML to be inserted
      */
-    var insertHTML = function(selector, html){
+    var insertHTML = function (selector, html) {
         var targetElement = document.querySelector(selector);
-        if(targetElement !== null){
+        if (targetElement !== null) {
             targetElement.innerHTML = html;
         }
     }
@@ -48,44 +50,44 @@
      * 
      * @param {*} selector the selector
      */
-    var showLoading = function(selector){
+    var showLoading = function (selector) {
 
         var html = "<div class='text-center'>";
         html += "<img src='../fine-dinning/resources/gifs/loading.gif'>";
         html += "</div>";
 
-        insertHTML(selector,html);
+        insertHTML(selector, html);
     }
 
     /**
      * Utility method to insert the carousel and cards into the main content
      */
-    fineDinning.loadHomePage = function(){
-        
+    fineDinning.loadHomePage = function () {
+
         activateTabAndDeactivateTheRest('at-home');
 
         var homeHTMLSnippet = "../fine-dinning/snippets/home-snippet.html";
-        $ajaxUtils.sendGetRequest(homeHTMLSnippet, 
-            function(responseText){
-                insertHTML("#main-content",responseText);
+        $ajaxUtils.sendGetRequest(homeHTMLSnippet,
+            function (responseText) {
+                insertHTML("#main-content", responseText);
                 // document.querySelector("#main-content").innerHTML = responseText;
             },
             false
         );
     }
-    
+
     /**
      * Utility method to load the about contents into the main content
      */
-    fineDinning.loadAboutPage = function(){
+    fineDinning.loadAboutPage = function () {
 
         activateTabAndDeactivateTheRest('at-about');
 
         var aboutHTMLSnippet = "../fine-dinning/snippets/about-snippet.html";
-        $ajaxUtils.sendGetRequest(aboutHTMLSnippet, 
-            function(responseText){
+        $ajaxUtils.sendGetRequest(aboutHTMLSnippet,
+            function (responseText) {
                 // document.querySelector("#main-content").innerHTML = responseText;
-                insertHTML("#main-content",responseText);
+                insertHTML("#main-content", responseText);
             },
             false
         );
@@ -94,54 +96,29 @@
     /**
      * Utility method to load the menu contents into the main content
      */
-    fineDinning.loadMenuPage = function(){
+    fineDinning.loadMenuPage = function () {
 
         activateTabAndDeactivateTheRest('at-menu');
 
         var menuHTMLSnippet = "../fine-dinning/snippets/menu-snippet.html";
-        $ajaxUtils.sendGetRequest(menuHTMLSnippet, 
-            function(responseText){
+        $ajaxUtils.sendGetRequest(menuHTMLSnippet,
+            function (responseText) {
 
-                insertHTML("#main-content",responseText);
-                var foodMenuDirectory = "../fine-dinning/resources/json/menu.json";
+                insertHTML("#main-content", responseText);
 
-                $ajaxUtils.sendGetRequest(foodMenuDirectory, 
-                    function(responseObjects){
-                        var breakfastFoodContents = renderFoodCardsForSchedule("breakfast", 
-                                                        responseObjects);    
-                        insertHTML("#breakfastBody", breakfastFoodContents);
-                    },
-                    true
-                );
-                $ajaxUtils.sendGetRequest(foodMenuDirectory, 
-                    function(responseObjects){
-                        var lunchFoodContents = renderFoodCardsForSchedule("lunch", 
-                                                    responseObjects);    
-                        insertHTML("#lunchBody", lunchFoodContents);
-                    },
-                    true
-                );
-                $ajaxUtils.sendGetRequest(foodMenuDirectory, 
-                    function(responseObjects){
-                        var dinnerFoodContents = renderFoodCardsForSchedule("dinner", 
-                                                    responseObjects);    
-                        insertHTML("#dinnerBody", dinnerFoodContents);
-                    },
-                    true
-                );
-                $ajaxUtils.sendGetRequest(foodMenuDirectory, 
-                    function(responseObjects){
-                        var dessertFoodContents = renderFoodCardsForSchedule("dessert", 
-                                                    responseObjects);    
-                        insertHTML("#dessertBody", dessertFoodContents);
-                    },
-                    true
-                );
-                $ajaxUtils.sendGetRequest(foodMenuDirectory, 
-                    function(responseObjects){
-                        var drinkFoodContents = renderFoodCardsForSchedule("drink", 
-                                                    responseObjects);    
-                        insertHTML("#drinkBody", drinkFoodContents);
+                $ajaxUtils.sendGetRequest(foodMenuDirectory,
+                    function (responseObjects) {
+
+                        insertHTML("#breakfastBody",
+                            renderFoodCardsForSchedule("breakfast", responseObjects));
+                        insertHTML("#lunchBody",
+                            renderFoodCardsForSchedule("lunch", responseObjects));
+                        insertHTML("#dinnerBody",
+                            renderFoodCardsForSchedule("dinner", responseObjects));
+                        insertHTML("#dessertBody",
+                            renderFoodCardsForSchedule("dessert", responseObjects));
+                        insertHTML("#drinkBody",
+                            renderFoodCardsForSchedule("drink", responseObjects));
                     },
                     true
                 );
@@ -151,19 +128,76 @@
     }
 
     /**
+     * Utility Method to retrieve the special dishes to be displayed at Specials page
+     */
+    fineDinning.loadSpecialsPage = function () {
+        activateTabAndDeactivateTheRest('at-menu');
+        var specialsHTMLSnippet = "../fine-dinning/snippets/specials-snippet.html";
+
+        function isIndexUsed(arrayOfUsedIndices, index) {
+            for (var counter = 0; counter < arrayOfUsedIndices.length; ++counter) {
+                if (index === arrayOfUsedIndices[counter]) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        $ajaxUtils.sendGetRequest(specialsHTMLSnippet,
+            function (responseText) {
+                insertHTML("#main-content", responseText);
+
+                $ajaxUtils.sendGetRequest(foodMenuDirectory,
+                    function (responseObjects) {
+
+                        var choicedDishes = {
+                            "breakfast": [],
+                            "lunch": [],
+                            "dinner": [],
+                            "dessert": [],
+                            "drink": [],
+                        };
+
+                        for (var element in responseObjects) {
+
+                            var usedIndex = [];
+                            var count = 0;
+
+                            while (count < 3) {
+                                //get random items from food categories
+                                var randomIndex = Math.floor(Math.random() * responseObjects[element].length);
+                                if (!isIndexUsed(usedIndex, randomIndex)) {
+                                    usedIndex.push(randomIndex);
+                                }
+                                count++;
+                            }
+                            choicedDishes[element].push(usedIndex);
+                        }
+
+                        insertHTML("#specialsBody", renderFoodCardsForSpecials(choicedDishes, responseObjects));
+                    },
+                    true
+                );
+            },
+            false
+        );
+
+    }
+
+    /**
      * utility method to activate new tab and deactivate the rest
      * 
      * @param {String} tabToActivate indicates the tab to activate
      */
-    var activateTabAndDeactivateTheRest = function(tabToActivate){
+    var activateTabAndDeactivateTheRest = function (tabToActivate) {
         const tabs = ['at-home', 'at-about', 'at-menu', 'at-news', 'at-contacts'];
         const activeState = 'active';
 
         var activePageIndicator = document.getElementById(tabToActivate);
         activePageIndicator.classList.add(activeState);
 
-        for(var counter = 0; counter < tabs.length; ++counter){
-            if(tabs[counter] !== tabToActivate){
+        for (var counter = 0; counter < tabs.length; ++counter) {
+            if (tabs[counter] !== tabToActivate) {
                 var inactivePageIndicator = document.getElementById(tabs[counter]);
                 inactivePageIndicator.classList.remove(activeState);
             }
@@ -175,32 +209,57 @@
      * 
      * @param {String} schedule (breakfast, lunch, dinner)
      * @param {Object} responseObjects (foods offered by the restaurant)
+     * @returns HTML to be rendered
      */
-    var renderFoodCardsForSchedule = function(schedule, responseObjects){
+    var renderFoodCardsForSchedule = function (schedule, responseObjects) {
+
         var foodContents = "";
 
-        for(var element in responseObjects[schedule]){
-
+        for (var element in responseObjects[schedule]) {
             foodContents += "<div>"
             foodContents += "<div class=\"card h-100\">";
-            foodContents += "<img src=\"" + responseObjects[schedule][element].directory 
-                                            +"\" class=\"card-img-top\" alt=\""
-                                            + responseObjects[schedule][element].alt +"\">";
-            foodContents += "<div class=\"card-body\">"
-            foodContents += "<h5 class=\"card-title\">" + responseObjects[schedule][element].name + "</h5>"
-            foodContents += "<p class=\"card-text\">" +  responseObjects[schedule][element].description + "</p>"
+            foodContents += "<img src=\"" + responseObjects[schedule][element].directory +
+                "\" class=\"card-img-top\" alt=\"" +
+                responseObjects[schedule][element].alt + "\">";
+            foodContents += "<div class=\"card-body\">";
+            foodContents += "<h5 class=\"card-title\">" + responseObjects[schedule][element].name + "</h5>";
+            foodContents += "<p class=\"card-text\">" + responseObjects[schedule][element].description + "</p>";
             foodContents += "</div></div></div>";
         }
 
         return foodContents;
     }
 
-  // Expose utility to the global object
-  global.$fineDinning = fineDinning;
+    /**
+     * utility method to generate random food items from the main menu to be presented as Special dishes.
+     * @param {*} choicedDishes randomly selected items from food categories
+     * @param {*} responseObjects food categories read from JSON file
+     * @returns HTML to be rendered
+     */
+    var renderFoodCardsForSpecials = function (choicedDishes, responseObjects) {
+
+        var specialsContents = "";
+
+        for (var choicedDish in choicedDishes) {
+            for (var element in choicedDishes[choicedDish]) {
+                for (var index in choicedDishes[choicedDish][element]) {
+                    specialsContents += "<div>"
+                    specialsContents += "<div class=\"card h-100\">";
+                    specialsContents += "<img src=\"" + responseObjects[choicedDish][index].directory +
+                        "\" class=\"card-img-top\" alt=\"" +
+                        responseObjects[choicedDish][index].alt + "\">";
+                    specialsContents += "<div class=\"card-body\">";
+                    specialsContents += "<h5 class=\"card-title\">" + responseObjects[choicedDish][index].name + "</h5>";
+                    specialsContents += "<p class=\"card-text\">" + responseObjects[choicedDish][index].description + "</p>";
+                    specialsContents += "</div></div></div>";
+                }
+            }
+        }
+        return specialsContents;
+    }
+
+    // Expose utility to the global object
+    global.$fineDinning = fineDinning;
 
 
 })(window);
-
-
-
-
